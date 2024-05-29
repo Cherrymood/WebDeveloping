96167b07f5d5dc3a94ab9b51e5a899b77783c2ee
Valeriia Horodnycha <valeriia.horodnycha@gmail.com>
2024-05-29 14:37:08 -0700

    Added QR-code project

--- Using Node/QR Code Project/index.js
+++ Using Node/QR Code Project/index.js
@@ -8,25 +8,31 @@ import inquirer from 'inquirer';
 import qr from 'qr-image';
 import fs from 'fs';
 
-inquirer
-  .prompt([
-    {message: "Type in your URL: ", 
-    name: "URL"}
-  ])
-  .then((answers) => {
+$(document).ready(function () {
+  $('.btn#generate').click(function (e) { 
+    
+    inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'URL',
+        message: 'Type in your URL: ',
+      },
+    ])
+    .then((answers) => {
+      // Use the URL input from the user
+      const url = answers.URL;
 
-    console.log(answers);
-    // Use user feedback for... whatever!!
-    const url = answers.URL;
+      var qr_svg = qr.image(url);
 
-    var qr_svg = qr.image(url);
-
-    qr_svg.pipe(fs.createWriteStream('my_qr.png'));
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
-  });
+      qr_svg.pipe(fs.createWriteStream('qr_img.png'));
+      })
+      .catch((error) => {
+        if (error.isTtyError) {
+          // Prompt couldn't be rendered in the current environment
+        } else {
+          // Something else went wrong
+        }
+      });
+    });
+});
