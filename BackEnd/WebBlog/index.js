1a87803766363e4da7b938ccb6474e09b325d2be
Valeriia Horodnycha <valeriia.horodnycha@gmail.com>
2024-06-11 16:10:59 -0700

    Added login and logout

--- BackEnd/WebBlog/index.js
+++ BackEnd/WebBlog/index.js
@@ -5,15 +5,18 @@ const port = 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
 
 const blogPosts = [
     { id: 1, title: 'Learning JavaScript'},
     { id: 2, title: 'Using MERN Stack'}
 ];
 
+let isAuthenticated = false;
+
 app.get('/', (req, res) => 
 {
-    res.render('../views/pages/home', { blogPosts });
+    res.render('../views/pages/home', { blogPosts, isAuthenticated});
 });
 
 app.get('/pages/:id', (req, res) =>
@@ -23,11 +26,30 @@ app.get('/pages/:id', (req, res) =>
 
     if(post)
     {
-        res.render(`pages/post${post.id}`, { post })
+        res.render(`pages/post${post.id}`, { post, isAuthenticated })
     }
     else
     {
         res.render(404).render('pages/404');
+    }
+});
+
+app.get('/login', (req, res) =>
+{
+    res.render('pages/login');
+});
+
+app.post('/login', (req, res) => {
+
+    const {username, password} = req.body;
+    if(username === 'john' && password === '123456')
+    {
+        isAuthenticated = true;
+        res.redirect('/');
+    }
+    else
+    {
+        res.redirect('/pagest/fail');
     }
 });
 
