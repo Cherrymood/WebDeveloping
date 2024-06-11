94278159652aa75285f1ef465465d81149e582e0
Valeriia Horodnycha <valeriia.horodnycha@gmail.com>
2024-06-11 15:34:08 -0700

    Created server

---
+++ BackEnd/WebBlog/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+
+const app = express();
+const post = 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+const blogPosts = [
+    { id: 1, title: 'Learning JavaScript'},
+    { id: 2, title: 'Using MERN Stack'}
+];
+
+app.get('/', (req, res) => 
+{
+    res.render('/pages/home', { blogPosts });
+});
+
+app.get('/pages/:id', (req, res) =>
+{
+    const postId = req.params.id;
+    const post = blogPosts.find((post) => post.id == postId);
+
+    if(post)
+    {
+        res.render(`pages/post${post.id}`, { post })
+    }
+    else
+    {
+        res.render(404).render('pages/404');
+    }
+});
+
+app.listen(port, () =>{
+    console.log(`Server is running on port ${port}`);
+});
