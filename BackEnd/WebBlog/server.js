9337a1f54bb65eced0f468bc12f1253249aec9cb
Valeriia Horodnycha <valeriia.horodnycha@gmail.com>
2024-06-12 14:16:54 -0700

    Added editor post

--- BackEnd/WebBlog/server.js
+++ BackEnd/WebBlog/server.js
@@ -1,11 +1,14 @@
 import express from "express";
+import fileupload from "express-fileupload";
 
 const app = express();
 const port = 3000;
 
+app.use(express.static("./public"));
+
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
+app.use(fileupload());
 app.use((req, res, next) => {
     res.locals.isAuthenticated = isAuthenticated;
     next();
@@ -30,41 +33,43 @@ app.get('/pages/:id', (req, res) =>
 
     if(post)
     {
-        res.render(`pages/post${post.id}`, { post, isAuthenticated })
+        res.render(`../views/pages/post${post.id}`, { post, isAuthenticated })
     }
     else
     {
-        res.render(404).render('pages/error');
+        res.redirect('../views/pages/error');
     }
+});
+
+app.get('/editor', (req, res) =>
+{
+    res.render('../views/pages/editor');
 });
 
 app.post('/comment/:id', (req, res) => {
     const postId = req.params.id;
     const post = blogPosts.find((post) => post.id == postId);
 
-    if(!isAuthenticated)
-    {
-        return res.redirect('/pages/error');
-    }
-
     if(post)
         {
             const comment = req.body.comment;
+            post.comments.push(comment);
+            
             if(comment)
             {
                 post.comments.push(comment);
             }
-            return res.redirect(`/pages/${postId}`);
+            return res.redirect(`../views/pages/${postId}`);
         }
     else
         {
-            return res.redirect('/pages/error');
+            return res.redirect('../views/pages/error');
         }
 });
 
 app.get('/login', (req, res) => 
 {
-    res.render('pages/login');
+    res.render('../views/pages/login');
 });
 
 app.post('/login', (req, res) => {
@@ -77,9 +82,32 @@ app.post('/login', (req, res) => {
     }
     else
     {
-        res.redirect('/pages/error');
+        res.redirect('../views/pages/error');
     }
 });
+
+//upload link 
+app.post('/upload', (req, res) => {
+    let file = req.files.image;
+    let date = new Date();
+    //image name
+    let imagename = date.getDate() + date.getTime() + file.name;
+    //image upload path
+    let path = 'public/uploads/'+ imagename;
+
+    //create upload
+    file.mv(path, (err, result) => {
+        if(err)
+        {
+            throw err;
+        }
+        else
+        {
+            // our image upload path
+            res.json(`uploads/${imagename}`);
+        }
+    });
+})
 
 app.listen(port, () =>{
     console.log(`Server is running on port ${port}`);
