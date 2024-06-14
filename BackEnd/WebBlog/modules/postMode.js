fba26a075e50db94ac9192e30c56c60aa8a51ba3
Valeriia Horodnycha <valeriia.horodnycha@gmail.com>
2024-06-14 13:55:35 -0700

    Added function, that return the data array of objects, if array exists.

--- BackEnd/WebBlog/modules/postMode.js
+++ BackEnd/WebBlog/modules/postMode.js
@@ -2,7 +2,20 @@ const fileName = '../data/post.json';
 let posts = require(fileName);
 const helper = require('../helper.js');
 
-function getPosts() {}
+function getPosts() {
+    
+    return new Promise((resolve, reject) => {
+        
+    if (posts.length === 0) {
+        reject({
+            message: 'no posts available',
+            status: 202
+        });
+    };
+    resolve(posts)
+    });
+};
+
 function getPost(id) {}
 function insertPost(newPost) {}
 function updatePost(id, newPost) {}
